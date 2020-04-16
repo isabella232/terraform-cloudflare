94b5edf5a94ae1f3813fee923c23695ea01de503
Jason Field <jason@avon-lea.co.uk>
2020-04-16 19:43:49 +0100

    Refactor to Zone

--- main.tf
+++ main.tf
@@ -2,14 +2,6 @@ provider "cloudflare" {
   version = "~> 2.0"
 }
 
-variable "souschefs_zone_id" {
-  default = "6455652de5d141a0db393d25310f1ef4"
-}
-
-variable "sous-chefs_zone_id" {
-  default = "UNKNOWN"
-}
-
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
--- souschefs.org.tf
+++ souschefs.org.tf
@@ -1,6 +1,12 @@
 # This domain is not not currently, but a dummy record is set
+
+resource "cloudflare_zone" "souschefs-org" {
+  zone = "souschefs.org"
+  plan = "free"
+}
+
 resource "cloudflare_record" "terraform" {
-  zone_id = var.souschefs_zone_id
+  zone_id = cloudflare_zone.souschefs-org.id
   name    = "terraform"
   value   = "192.168.0.1"
   type    = "A"
